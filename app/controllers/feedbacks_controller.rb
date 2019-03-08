62fbf151ecd348f30372f338070a8a3a27e66975
abduqah <abdulrahman.hamdy@orchtech,com>
2019-03-08 16:00:42 +0200

    Post and Get

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,18 +1,26 @@
 class FeedbacksController < ApplicationController
+	before_action :set_company_token
 
 	def index
+		Feedback.all
 	end
 
 	def show
-	end
-
-	def new
+		Feedback.where(number: params[:id], company_token: company_token)
 	end
 
 	def create
-	end
+		@feedback = Feedback.new(feedback_params, company_token: company_token)
+		@state = State.new(state_params)
+		@feedback.state = @state
 
-	def update
+		rv = (@feedback.save! && @state.save!)
+
+		if rv
+			render json: {number: @feedback.number}, status: :created	
+		else
+			render json: @feedback.errors? ? @feedback.errors : @state.errors, status: :unprocessable_entity
+		end
 	end
 
 	def count
@@ -21,10 +29,11 @@ class FeedbacksController < ApplicationController
 	private
 
 		def set_company_token
+			company_token = params[:company_token]
 		end
 
 		def feedback_params
-			params.require(:feedback).permit(:company_token, :number, :priority)
+			params.require(:feedback).permit(:priority)
 		end
 
 		def state_params
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,8 +1,22 @@
 class Feedback < ApplicationRecord
+	extend Enumerize
+
+	default_scope { order(created_at: :desc) }
+
 	has_one :state, dependent: :destroy
 	validates_presence_of :company_token, :number, :priority
 	validates_uniqueness_of :company_token
 	validates_uniqueness_of :number, scope: :company_token
 	enumerize :priority, in: { minor: 1, major: 2, critical: 3 }
-	validates_inclusion_of :priority, in: 1..3, message: 'priority must be in 1 for minor, 2 for major or 3 for critical'
+	validates_inclusion_of :priority, in: 1..3, message: 'priority must be in ( 1, 2, 3 ) for minor, major or critical respectively'
+
+	after_create :generate_number
+	
+	def generate_number
+		# this implementation is trivial for no recored shall be deleted
+		# numbers start from 1
+		last_feedback = Feedback.where(company_token: company_token).first
+		last_number = last_feedback? ? last_feedback.number : 0
+		update_column(number, last_number + 1)
+	end
 end
