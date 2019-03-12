d7d24afa964a2dc9f5c01aed838d4079cfe6a0e6
abduqah <abdulrahman.hamdy@orchtech,com>
2019-03-12 08:12:44 +0200

    M number caching worker

--- app/workers/feedback_number_cache_worker.rb
+++ app/workers/feedback_number_cache_worker.rb
@@ -3,12 +3,9 @@ class FeedbackNumberCacheWorker
 	require "redis"
 
 	include Sidekiq::Worker
-	sidekiq_options retry: false
 
 	def perform(company_token)
 		redis = Redis.new
-		
-		redis.set(company_token, Feedback.search(company_token,	fields: [{company_token: :exact}]).size)
-		
+		redis.set(company_token, Feedback.search(company_token || '*',	fields: [{company_token: :exact}]).size)
 	end
 end
